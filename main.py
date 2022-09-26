b51f5e18418810f4e9f86c00526220713a084a39
legatofseal@gmail.com <Trigonometria12*>
2022-09-26 19:13:41 +0300

    Init

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from math import sin
 from random import uniform
 
 class organism():
-    def __init__(self, settings, wih=None, who=None, name=None):
+    def __init__(self, settings, neural_matrix = None, name=None):
 
         self.x = uniform(settings['x_min'], settings['x_max'])  # position (x)
         self.y = uniform(settings['y_min'], settings['y_max'])  # position (y)
@@ -17,34 +17,41 @@ class organism():
         self.r = uniform(0,360)                 # orientation   [0, 360]
 
         self.speed = 0.01
+        self.v = self.speed
         self.size = 10
         self.health = self.size
         self.food_count = self.size/2  # fitness (food count)
         self.layers_number = 2
         self.neurons_number_in_layer = 5
         self.name = name
+        self.intelegence = 1
+        self.neural_matrix = neural_matrix
+        if not neural_matrix:
+            self.neural_matrix = []
+            self.neural_matrix.append(np.random.uniform(-1, 1, self.neurons_number_in_layer,  self.intelegence))
+            for i in range(self.layers_number-2):
+                self.neural_matrix.append(np.random.uniform(-1, 1, self.neurons_number_in_layer, self.neurons_number_in_layer))
+            self.neural_matrix.append(np.random.uniform(-1, 1, self.neurons_number_in_layer, 1)) #only direction for now
 
-        #wih_init = np.random.uniform(-1, 1, (settings['hnodes'], settings['inodes']))  # mlp weights (input -> hidden)
-        #who_init = np.random.uniform(-1, 1, (settings['onodes'], settings['hnodes']))
         self.d_food = 100   # distance to nearest food
         self.r_food = 0     # orientation to nearest food
 
 
-        self.wih = wih
-        self.who = who
 
 
     # NEURAL NETWORK
-    def think(self):
+    def think(self, feature_vector):
 
         # SIMPLE MLP
         af = lambda x: np.tanh(x)               # activation function
-        h1 = af(np.dot(self.wih, self.r_food))  # hidden layer
-        out = af(np.dot(self.who, h1))          # output layer
+        feature_slice = feature_vector[0:self.intelegence]
+        hl = af(np.dot(self.neural_matrix[0], self.r_food))
+        for i in range(1,len(self.neural_matrix)-2):
+            hl = af(np.dot(self.neural_matrix[i], hl))
+        out =  af(np.dot(np.dot(self.neural_matrix[self.neural_matrix[-1]], hl)))
 
-        # UPDATE dv AND dr WITH MLP RESPONSE
-        self.nn_dv = float(out[0])   # [-1, 1]  (accelerate=1, deaccelerate=-1)
-        self.nn_dr = float(out[1])   # [-1, 1]  (left=1, right=-1)
+
+        self.nn_dr = float(out)   # [-1, 1]  (left=1, right=-1)
 
 
     # UPDATE HEADING
@@ -55,9 +62,10 @@ class organism():
 
     # UPDATE VELOCITY
     def update_vel(self, settings):
-        self.v += self.nn_dv * settings['dv_max'] * settings['dt']
-        if self.v < 0: self.v = 0
-        if self.v > settings['v_max']: self.v = settings['v_max']
+        #self.v += self.nn_dv * settings['dv_max'] * settings['dt']
+        #if self.v < 0: self.v = 0
+        #if self.v > settings['v_max']: self.v = settings['v_max']
+        self.v =  self.speed
 
 
     # UPDATE POSITION
@@ -66,3 +74,17 @@ class organism():
         dy = self.v * sin(radians(self.r)) * settings['dt']
         self.x += dx
         self.y += dy
+
+class food():
+    def __init__(self, settings):
+        self._x = uniform(settings['x_min'], settings['x_max'])
+        self._y = uniform(settings['y_min'], settings['y_max'])
+        self._energy = random.randint(1,5)
+
+    def _get_energy(self):
+        return self._radius
+    def _get_x(self):
+        return self._x
+    def _get_y(self):
+        return self._y
+
